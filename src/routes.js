b8a0a2ed19a3dd135b879d840739deafdb277c59
David Strayhorn <david.strayhorn4@gmail.com>
2024-09-27 01:28:55 -0400

    add main dashboard page; fix breadcrumbs pathname

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import routesMain from './routes/routesMain'
 import routesHelloWorld from 'src/routes/routesHelloWorld'
 import routesSettings from 'src/routes/routesSettings'
 import routesApp1 from 'src/routes/routesApp1'
@@ -5,6 +6,7 @@ import routesApp2 from 'src/routes/routesApp2'
 import routesApp3 from 'src/routes/routesApp3'
 
 const routes = [
+  ...routesMain,
   ...routesHelloWorld,
   ...routesSettings,
   ...routesApp1,
